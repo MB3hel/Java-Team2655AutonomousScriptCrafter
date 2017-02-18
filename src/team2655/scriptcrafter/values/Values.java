6b2f732c86b9589df5b4629ca4b2169c05cff70e
Marcus Behel <Marcus Behel>
2017-02-18 14:51:36 -0500

    Changed Version to 2.1.0

--- src/team2655/scriptcrafter/values/Values.java
+++ src/team2655/scriptcrafter/values/Values.java
@@ -3,8 +3,8 @@ package team2655.scriptcrafter.values;
 public interface Values {
 	
 	public final int VERSION_MAJOR = 2;
-	public final int VERSION_MINOR = 0;
-	public final int VERSION_BUILD = 3;
+	public final int VERSION_MINOR = 1;
+	public final int VERSION_BUILD = 0;
 	
 	public final String RELEASE_TYPE = "";
 	public final String ARGUMENT_TYPE_NUMERIC = "Numeric";
